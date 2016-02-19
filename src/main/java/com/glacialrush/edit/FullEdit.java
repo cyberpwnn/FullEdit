9b516700b4ff1d3d197fa097468670cb90eba1f0
danielmills <danielmillst@gmail.com>
2016-02-18 22:29:37 -0500

    Create Initial Plugin file

---
+++ src/main/java/com/glacialrush/edit/FullEdit.java
@@ -0,0 +1,16 @@
+package com.glacialrush.edit;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class FullEdit extends JavaPlugin
+{
+	public void onEnable()
+	{
+		
+	}
+	
+	public void onDisable()
+	{
+		
+	}
+}
